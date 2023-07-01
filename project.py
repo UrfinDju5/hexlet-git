e9f96c041686dcfaaa8f3ebd3be417548554dd66
Valdemar1988 <qwertysenator@gmail.com>
2023-07-01 18:44:04 +0300

    new file for my repository

---
+++ project.py
@@ -0,0 +1,27 @@
+
+ef f
+d_index(value, items):
+    for index, item in enumerate(ite):
+        if item == value:
+            return index
+
+
+def find_second_index(value, items):
+    count = iter(items)
+    first = find_index(value, count)
+    second = find_index(value, count)
+    if first is None:
+        return None
+    elif second is None:
+        return None
+    else:
+        return first + secodiwq
+    q
+    nd + 1
+
+
+print(find_second_index('b', 'bob'))
+print(find_second_index('a', 'cat'))
+q
+/
+
